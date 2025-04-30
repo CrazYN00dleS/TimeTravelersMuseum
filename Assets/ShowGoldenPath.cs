5544d90c172f536cd4a8a1db35b5f4107becd226
Yifan Shen <terrys19990@gmail.com>
2025-04-30 08:39:36 -0400

    Added Navmesh surface, agent, simple navigation lines
    1. Added and Baked Navmesh Surface
    2. Added NavMesh Agent to the player
    3. Added simple navigation line to the player
    4. change glass door to be ignored for navigation
    TODO need to chagne all doors to be ignored

--- Assets/ShowGoldenPath.cs
+++ Assets/ShowGoldenPath.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+
+
+[RequireComponent(typeof(LineRenderer))]
 public class ShowGoldenPath : MonoBehaviour
 {
 
     public Transform target;
     private NavMeshPath path;
+    private LineRenderer lineRenderer;
     private float elapsed = 0.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         path = new NavMeshPath();
         elapsed = 0.0f;
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startColor = Color.yellow;
+        lineRenderer.endColor = Color.yellow;
+        lineRenderer.positionCount = 0;
     }
 
     // Update is called once per frame
@@ -23,10 +34,23 @@ public class ShowGoldenPath : MonoBehaviour
         {
             elapsed -= 1.0f;
             NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path);
+            UpdateLineRenderer();
         }
         for (int i = 0; i < path.corners.Length - 1; i++)
         {
             Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
         }
+    }
+
+    void UpdateLineRenderer()
+    {
+        if (path.corners.Length < 2)
+        {
+            lineRenderer.positionCount = 0;
+            return;
+        }
+
+        lineRenderer.positionCount = path.corners.Length;
+        lineRenderer.SetPositions(path.corners);
     }
 }
